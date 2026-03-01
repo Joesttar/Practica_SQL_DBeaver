7b9da9d956036261d0468750e1e17d8b5cd6bc86
Joe Garcia <joegarcia12_al@hotmail.com>
2026-03-01 11:19:03 -0600

    Codigo de limpieza

--- limpieza_de_data.py
+++ limpieza_de_data.py
@@ -9,6 +9,22 @@ def limpieza_texto(texto):
         return texto  # Si no es un string, lo devolvemos tal cual
 
 # Eliminar acentos
-    texto = unicodedata.normalize('NFD', texto).encode ('ascii', 'ignore').decode('utf-8')
+texto_limpio = unicodedata.normalize('NFD', texto).encode ('ascii', 'ignore').decode('utf-8')
 
 # Eliminar espacios extra y aparte agregamos Capitalize 
+return texto_limpio.strip().capitalize()
+
+#----- 1. Limpieza en la tabla pordutos -------
+# Corregir errores ortograficos
+df_prodcutos['categoria'] = df_prodcutos['categoria'].replace('Hogas', 'Hogar')
+
+# Normalizacion masiva de categorias
+df_prodcutos['categoria'] = df_prodcutos['categoria'].apply(limpieza_texto)
+df_prdocutos['producto_nombre'] = df_prdocutos['producto_nombre'].apply(limpieza_texto)
+
+#----- 2. Limpieza en la tabla clientes -------
+# Convertir fechas a formato datetime de Pandas
+df_ventas['fecha_venta'] = pd.to_datetime(df_ventas['fecha_ventas'])
+
+# Llenas montos totales nulos (si existen) multiplicando cantidad * precio
+
