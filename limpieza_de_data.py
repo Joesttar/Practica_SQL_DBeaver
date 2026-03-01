0486c850e573ba765f63743adc34aaf3e2154b79
Joe Garcia <joegarcia12_al@hotmail.com>
2026-03-01 11:25:47 -0600

    Se hiz cambios a errores ortograficos

--- limpieza_de_data.py
+++ limpieza_de_data.py
@@ -9,22 +9,33 @@ def limpieza_texto(texto):
         return texto  # Si no es un string, lo devolvemos tal cual
 
 # Eliminar acentos
-texto_limpio = unicodedata.normalize('NFD', texto).encode ('ascii', 'ignore').decode('utf-8')
+    texto_limpio = unicodedata.normalize('NFD', texto).encode ('ascii', 'ignore').decode('utf-8')
 
-# Eliminar espacios extra y aparte agregamos Capitalize 
-return texto_limpio.strip().capitalize()
+# Eliminar espacios extra
+    return texto_limpio.strip().capitalize()
 
 #----- 1. Limpieza en la tabla pordutos -------
 # Corregir errores ortograficos
-df_prodcutos['categoria'] = df_prodcutos['categoria'].replace('Hogas', 'Hogar')
+df_productos['categoria'] = df_productos['categoria'].replace('Hogas', 'Hogar')
 
 # Normalizacion masiva de categorias
-df_prodcutos['categoria'] = df_prodcutos['categoria'].apply(limpieza_texto)
-df_prdocutos['producto_nombre'] = df_prdocutos['producto_nombre'].apply(limpieza_texto)
+df_productos['categoria'] = df_productos['categoria'].apply(limpieza_texto)
+df_productos['producto_nombre'] = df_productos['producto_nombre'].apply(limpieza_texto)
 
 #----- 2. Limpieza en la tabla clientes -------
 # Convertir fechas a formato datetime de Pandas
-df_ventas['fecha_venta'] = pd.to_datetime(df_ventas['fecha_ventas'])
+df_ventas['fecha_venta'] = pd.to_datetime(df_ventas['fecha_venta'])
 
 # Llenas montos totales nulos (si existen) multiplicando cantidad * precio
+# Para esto unimos temporamente con productos
+df_ventas = df_ventas.merge(df_prodcutos[['prodcuto_id', 'precio_unitario']], on='producto_id', how='left')
+df_ventas['monto total'] = df_ventas['monto_total'].fillna(df_ventas['cantidad'] * df_ventas['precio_unitario'])
+
+# ----- 3. Limpieza en la tabla usuario -------
+df_usuario['nombre'] = df_usuario['nombre'].apply(limpieza_texto)
+df_usuario['rol'] = df_usuario['rol'].str.strip().str.lower()  # Normalizamos el rol a minusculas y sin espacios
+
+print("Limpieza de datos completada exitosamente.")
+print("\nCategorias unicas detectadas:", df_productos['categoria'].unique())
+print(df_productos.head)
 
